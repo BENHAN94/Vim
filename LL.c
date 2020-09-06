f8964088a30ce1d814f9be948d899b187e0b4c3d
My name <dkfmaek333@gmail.com>
2020-09-06 20:00:26 +0900

    Rsearch

--- LL.c
+++ LL.c
@@ -48,9 +48,69 @@ int Count(struct Node *p)
     }
     return c;
 }
+int Sum(struct Node *p){
+    int s=0;
+    while(p){
+        s+=p->data;
+        p=p->next;
+    }
+    return s;
+}
+int Rsum(struct Node *p){
+    if(p){
+        return Rsum(p->next)+p->data;
+    }
+    return 0;
+}
+int Max(struct Node *p){
+    int M;
+    M=p->data;
+    while(p){
+        if(M<p->data){
+            M=p->data;
+        }
+        p=p->next;
+    }
+    return M;
+}
+int Rmax(struct Node *p){
+    int x=0;
+    if(!p){
+        return INT32_MIN;
+    } 
+    x=Rmax(p->next);
+    if(x>p->data){
+        return x;
+    }else{
+        return p->data;
+    }
+}
+struct Node *Search(struct Node *p, int x){
+    while(p){
+        if(p->data==x){
+            return p;
+        }
+        else{
+            p=p->next;
+        }
+    }
+    return p;
+}
+struct Node *Rsearch(struct Node *p, int x){
+    if(p){
+        if(p->data==x){
+           return p; 
+        }
+        else{
+            return Rsearch(p->next,x);
+        }
+    }
+    return NULL;
+}
+
 int main(){
     int A[]={3,5,7,10,15};
     create(A, 5);
-    printf("%d",Count(first));
+    printf("%p",Rsearch(first, 15));
     return 0;
 }
